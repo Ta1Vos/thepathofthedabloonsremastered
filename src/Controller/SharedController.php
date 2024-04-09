a55454d0b6050b0658e0d3d1327ec4091e46c04c
Ta1Vos <302196386@student.rocmondriaan.nl>
2024-04-09 13:56:24 +0200

    set up controllers

---
+++ src/Controller/SharedController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class SharedController extends AbstractController
+{
+    #[Route('/shared', name: 'app_shared')]
+    public function index(): Response
+    {
+        return $this->render('shared/index.html.twig', [
+            'controller_name' => 'SharedController',
+        ]);
+    }
+}
