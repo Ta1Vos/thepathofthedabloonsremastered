c18b6f9e1b366a4167fbe69627b62ed57f741001
Ta1Vos <302196386@student.rocmondriaan.nl>
2024-05-16 09:23:48 +0200

    added form to member search

--- src/Controller/ModeratorController.php
+++ src/Controller/ModeratorController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UsernameType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -16,7 +18,6 @@ class ModeratorController extends AbstractController
     {
         $this->denyAccessUnlessGranted('ROLE_MODERATOR');
         return $this->render('moderator/dashboard.html.twig', [
-            'controller_name' => 'ModeratorController',
             'bannerTitle' => "TPOTDR | MODERATOR",
         ]);
     }
@@ -24,13 +25,27 @@ class ModeratorController extends AbstractController
     #[Route('/moderator/member-search', name: 'app_mod_member_searching')]
     public function modMemberSearch(Request $request, EntityManagerInterface $entityManager): Response
     {
-        $userList = $entityManager->getRepository(User::class)->findAll();
-
         $this->denyAccessUnlessGranted('ROLE_MODERATOR');
-        return $this->render('moderator/member_editing.html.twig', [
-            'controller_name' => 'ModeratorController',
+        $userList = [];
+
+        $form = $this->createForm(UsernameType::class);
+        $form->add('search_all', SubmitType::class, [
+            'attr' => [
+                'label' => 'Search all'
+            ]
+        ]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //If search all
+            $userList = $entityManager->getRepository(User::class)->findAll();
+            //If search one
+        }
+
+        return $this->render('moderator/member_search.html.twig', [
             'bannerTitle' => "TPOTDR | SEARCH",
             'users' => $userList,
+            'form' => $form
         ]);
     }
 
@@ -39,7 +54,6 @@ class ModeratorController extends AbstractController
     {
         $this->denyAccessUnlessGranted('ROLE_MODERATOR');
         return $this->render('moderator/member_editing.html.twig', [
-            'controller_name' => 'ModeratorController',
             'bannerTitle' => "TPOTDR | MODERATE",
         ]);
     }
