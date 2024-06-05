a26676f8a0d03e7dfc78a0e7a9e49d1789e8f53e
Ta1Vos <302196386@student.rocmondriaan.nl>
2024-06-05 11:46:37 +0200

    added WorldType & RarityType

---
+++ src/Form/WorldType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Event;
+use App\Entity\World;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class WorldType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('distanceLimit')
+            ->add('events', EntityType::class, [
+                'class' => World::class,
+                'choice_label' => function (World $entity) {
+                    return $entity->getId() . ': ' . $entity->getName();
+                },
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => World::class,
+        ]);
+    }
+}
