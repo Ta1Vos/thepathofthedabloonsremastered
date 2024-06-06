53341a88506567ea3caa286080ce3a9975c71fea
Ta1Vos <302196386@student.rocmondriaan.nl>
2024-06-06 08:53:17 +0200

    added world fixtures

--- src/DataFixtures/WorldFixtures.php
+++ src/DataFixtures/WorldFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\World;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -11,6 +12,58 @@ class WorldFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+        $world = new World();
+        $world->setName('The Forest');
+        $world->setDistanceLimit(0);
+        $manager->persist($world);
+
+        $world->setName('The Village');
+        $world->setDistanceLimit(12);
+        $manager->persist($world);
+
+        $world->setName('The Dark Forest');
+        $world->setDistanceLimit(12);
+        $manager->persist($world);
+
+        $world->setName('Rolling Hills');
+        $world->setDistanceLimit(25);
+        $manager->persist($world);
+
+        $world->setName('Freezing Taiga');
+        $world->setDistanceLimit(35);
+        $manager->persist($world);
+
+        $world->setName('IceHeart Village');
+        $world->setDistanceLimit(40);
+        $manager->persist($world);
+
+        $world->setName('BlueIce Valley');
+        $world->setDistanceLimit(45);
+        $manager->persist($world);
+
+        $world->setName('Barren Flatlands');
+        $world->setDistanceLimit(55);
+        $manager->persist($world);
+
+        $world->setName('Peaking Mountains');
+        $world->setDistanceLimit(75);
+        $manager->persist($world);
+
+        $world->setName('Traveler\'s Inn.');
+        $world->setDistanceLimit(85);
+        $manager->persist($world);
+
+        $world->setName('Point of No Return');
+        $world->setDistanceLimit(85);
+        $manager->persist($world);
+
+        $world->setName('Ash Hills');
+        $world->setDistanceLimit(86);
+        $manager->persist($world);
+
+        $world->setName('Temple of the Molten Dabloons');
+        $world->setDistanceLimit(100);
+        $manager->persist($world);
 
         $manager->flush();
     }
