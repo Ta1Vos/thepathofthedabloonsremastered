305654524e3563078c3c7267562c73120b3af2a0
Ta1Vos <302196386@student.rocmondriaan.nl>
2024-06-10 14:59:05 +0200

    added new item fixture

--- src/DataFixtures/ItemFixtures.php
+++ src/DataFixtures/ItemFixtures.php
@@ -3,16 +3,43 @@
 namespace App\DataFixtures;
 
 use App\Entity\Item;
+use App\Entity\Rarity;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class ItemFixtures extends Fixture
+class ItemFixtures extends Fixture implements OrderedFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $common = $manager->getRepository(Rarity::class)->findOneBy(['name' => 'Common']);
+        $heal10 = $manager->getRepository(Rarity::class)->findOneBy(['name' => 'heal 10 hp']);
+
+         $item = new Item();
+         $item->setName('Twig');
+         $item->setPrice(5);
+         $item->setIsWeapon(true);
+         $item->setDefeatChance(30);
+         $item->setRarity($common);
+//         $item->addEffect(null);
+        $item->setDescription("{$item->getDefeatChance()}% chance to fend off enemies. But seriously, it's literally a twig.");
+         $manager->persist($item);
+
+        $item = new Item();
+        $item->setName('Sandwich');
+        $item->setPrice(2);
+        $item->setIsWeapon(false);
+//        $item->setDefeatChance(30);
+        $item->setRarity($common);
+         $item->addEffect($heal10);
+        $item->setDescription("Heal {$heal10->getDebuffSeverity()} hp. Yummy!");
+        $manager->persist($item);
 
         $manager->flush();
+    }
+
+    public function getOrder(): int
+    {
+        return 10; // This will be loaded first
     }
 }
