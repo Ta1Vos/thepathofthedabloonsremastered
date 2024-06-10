1e86fdc7a5d13fbde9124ee79b7131ddb0990450
Ta1Vos <302196386@student.rocmondriaan.nl>
2024-06-10 14:17:32 +0200

    added help fields for Rarity inputs

--- src/Form/RarityType.php
+++ src/Form/RarityType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Rarity;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,7 +15,11 @@ class RarityType extends AbstractType
         $builder
             ->add('name')
             ->add('chanceIn', null, [
-                'label' => 'Chance in (... in 100)'
+                'label' => 'Chance in',
+                'help' => '{Chance}% in 100%'
+            ])
+            ->add('priority', NumberType::class, [
+                'help' => 'Priority = The lowest number will be used first. Example: If Common(1) fails, it will try Uncommon(2), then Rare(3), etc..'
             ])
         ;
     }
